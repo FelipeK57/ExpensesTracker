d99bb0eab77448db03208fb76b26fc501034a496
Kevin Felipe Bolaños Orozco <kevinboor5725@gmail.com>
2025-06-11 22:14:22 -0500

    Actualizar título de la aplicación a "Fin.Track" y agregar funcionalidad de filtrado en el historial de transacciones. Se implementó un selector para elegir entre todos, ingresos y egresos, y se mejoró el manejo de registros vacíos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,47 @@
+import { useState } from "react";
 import { AddExpense } from "./components/AddExpense";
 import { AddIncome } from "./components/AddIncome";
 import { BalanceCard } from "./components/BalanceCard";
+import { DeleteHistory } from "./components/DeleteHistory";
 import { History } from "./components/History";
-import { useStore } from "./store";
 
 export const App = () => {
-  const { cleanTransactions } = useStore();
+  const [selection, setSelection] = useState<"all" | "income" | "expense">(
+    "all"
+  );
 
   return (
     <main className="flex flex-col gap-2 p-4 h-screen max-w-sm mx-auto">
-      <h1 className="text-xl font-semibold text-center">MiEconomia</h1>
+      <h1 className="text-transparent w-fit mx-auto bg-clip-text bg-gradient-to-r from-zinc-400 via-zinc-200 to-zinc-400 font-semibold text-center">
+        Fin.Track
+      </h1>
       <BalanceCard />
       <div className="flex gap-2 justify-betweenw-full">
         <AddIncome />
         <AddExpense />
       </div>
       <h2 className="text-sm font-light text-zinc-400">
-        Historial de movimientos
+        Historial de registros
       </h2>
-      <History />
-      <button onClick={cleanTransactions} className="text-sm border-1 border-zinc-500 rounded-lg p-2 cursor-pointer">
-        Eliminar historial
-      </button>
+      <select
+        value={selection}
+        onChange={(e) =>
+          setSelection(e.target.value as "all" | "income" | "expense")
+        }
+        className="text-sm border-1 border-zinc-800 rounded-lg p-2 cursor-pointer bg-zinc-900 text-zinc-400 mx-1"
+      >
+        <option className="w-full p-2 bg-zinc-900" value="all">
+          Todas
+        </option>
+        <option className="w-full p-2 bg-zinc-900" value="income">
+          Ingresos
+        </option>
+        <option className="w-full p-2 bg-zinc-900" value="expense">
+          Egresos
+        </option>
+      </select>
+      <History selection={selection} />
+      <DeleteHistory />
     </main>
   );
 };
